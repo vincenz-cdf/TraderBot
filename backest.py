5ed1b88d15d226b60a647581ac4c753f18e04ba3
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-04-19 18:41:28 +0200

    Update backest.py

--- backest.py
+++ backest.py
@@ -1,19 +1,18 @@
 import pandas as pd
 from binance.client import Client
 import ta
+import matplotlib.pyplot as plt
 
-# Importation des données de Binance sur le BTC / USDT
-klinesT = Client().get_historical_klines("BTCUSDT",
-                                         Client.KLINE_INTERVAL_1HOUR,
-                                         "01 January 2017")
-df = pd.DataFrame(klinesT,
-                  columns=[
-                      'timestamp', 'open', 'high', 'low', 'close', 'volume',
-                      'close_time', 'quote_av', 'trades', 'tb_base_av',
-                      'tb_quote_av', 'ignore'
-                  ])
+client = Client()
 
-# Filtration des données importantes
+klinesT = client.get_historical_klines("ETHEUR", Client.KLINE_INTERVAL_1HOUR, "01 january 2020")
+
+df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
+df['close'] = pd.to_numeric(df['close'])
+df['high'] = pd.to_numeric(df['high'])
+df['low'] = pd.to_numeric(df['low'])
+df['open'] = pd.to_numeric(df['open'])
+
 del df['ignore']
 del df['close_time']
 del df['quote_av']
@@ -21,40 +20,91 @@ del df['trades']
 del df['tb_base_av']
 del df['tb_quote_av']
 
-df['close'] = pd.to_numeric(df['close'])
-df['high'] = pd.to_numeric(df['high'])
-df['low'] = pd.to_numeric(df['low'])
-df['open'] = pd.to_numeric(df['open'])
-
-# Convertir la date
 df = df.set_index(df['timestamp'])
 df.index = pd.to_datetime(df.index, unit='ms')
-
 del df['timestamp']
 
-#Definition des stratégies : moyenne mobile simple 200/600
-df['SMA200'] = ta.trend.sma_indicator(df['close'], 200)
-df['SMA600'] = ta.trend.sma_indicator(df['close'], 600)
+#Definition des stratégies : moyenne mobile exponentielle 28/48 + RSI
+dfTest = df.copy()
+dfTest['EMA28']=ta.trend.ema_indicator(df['close'], 28)
+dfTest['EMA48']=ta.trend.ema_indicator(df['close'], 48)
+dfTest['STOCH_RSI']=ta.momentum.stochrsi(df['close'])
+dfTest
 
 # Realisation d'un Backtest
-usdt = 1000
-btc = 0
+dt = None
+dt = pd.DataFrame(columns = ['date','position', 'price', 'frais' ,'fiat', 'coins', 'wallet', 'drawBack'])
+
+eur = 1000
+initalWallet = eur
+coin = 0
+wallet = 1000
+lastAth = 0
 lastIndex = df.first_valid_index()
+fee = 0.0007
 
-for index, row in df.iterrows():
-  if df['SMA200'][lastIndex] > df['SMA600'][lastIndex] and usdt > 10:
-    btc = usdt / df['close'][index]
-    btc = btc - 0.0007 * btc # Frais d'achat
-    usdt = 0
-    print("Buy BTC at",df['close'][index],'$ the',index)
+for index, row in dfTest.iterrows():
+  #Buy
+  if row['EMA28'] > row['EMA48'] and row['STOCH_RSI'] < 0.8 and  eur > 0:
+    coin = eur / row['close']
+    frais = fee * coin
+    coin = coin - frais
+    eur = 0
+    wallet = coin * row['close']
+    if wallet > lastAth:
+      lastAth = wallet
+    # print("Buy COIN at",df['close'][index],'$ the', index)
+    myrow = {'date': index,'position': "Buy",'price': row['close'],'frais': frais * row['close'],'fiat': eur,'coins': coin,'wallet': wallet,'drawBack':(wallet-lastAth)/lastAth}
+    dt = dt.append(myrow,ignore_index=True)
 
-  if df['SMA200'][lastIndex] < df['SMA600'][lastIndex] and btc > 0.0001:
-    usdt = btc * df['close'][index]
-    usdt = usdt - 0.0007 * usdt
-    btc = 0
-    print("Sell BTC at",df['close'][index],'$ the',index)
+  #Sell
+  if row['EMA28'] < row['EMA48'] and row['STOCH_RSI'] > 0.2 and coin > 0:
+    eur = coin * row['close']
+    frais = fee * eur
+    eur = eur - frais
+    coin = 0
+    wallet = eur
+    if wallet > lastAth:
+      lastAth = wallet
+    # print("Sell COIN at",df['close'][index],'$ the', index)
+    myrow = {'date': index,'position': "Sell",'price': row['close'],'frais': frais,'fiat': eur,'coins': coin,'wallet': wallet,'drawBack':(wallet-lastAth)/lastAth}
+    dt = dt.append(myrow,ignore_index=True)
+  
   lastIndex = index
 
-# Resultat des courses
-finalResult = usdt + btc * df['close'].iloc[-1]
-print("Final result",finalResult,'USDT')
+#///////////////////////////////////////
+print("Period : [" + str(dfTest.index[0]) + "] -> [" +str(dfTest.index[len(dfTest)-1]) + "]")
+dt = dt.set_index(dt['date'])
+dt.index = pd.to_datetime(dt.index)
+dt['resultat'] = dt['wallet'].diff()
+dt['resultat%'] = dt['wallet'].pct_change()*100
+dt.loc[dt['position']=='Buy','resultat'] = None
+dt.loc[dt['position']=='Buy','resultat%'] = None
+
+dt['tradeIs'] = ''
+dt.loc[dt['resultat']>0,'tradeIs'] = 'Good'
+dt.loc[dt['resultat']<=0,'tradeIs'] = 'Bad'
+
+iniClose = dfTest.iloc[0]['close']
+lastClose = dfTest.iloc[len(dfTest)-1]['close']
+holdPorcentage = ((lastClose - iniClose)/iniClose) * 100
+algoPorcentage = ((wallet - initalWallet)/initalWallet) * 100
+vsHoldPorcentage = ((algoPorcentage - holdPorcentage)/holdPorcentage) * 100
+
+print("Starting balance : 1000 $")
+print("Final balance :",round(wallet,2),"$")
+print("Performance vs US Dollar :",round(algoPorcentage,2),"%")
+print("Buy and Hold Performence :",round(holdPorcentage,2),"%")
+print("Performance vs Buy and Hold :",round(vsHoldPorcentage,2),"%")
+print("Number of negative trades : ",dt.groupby('tradeIs')['date'].nunique()['Bad'])
+print("Number of positive trades : ",dt.groupby('tradeIs')['date'].nunique()['Good'])
+print("Average Positive Trades : ",round(dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].sum()/dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].count(),2),"%")
+print("Average Negative Trades : ",round(dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].sum()/dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].count(),2),"%")
+idbest = dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].idxmax()
+idworst = dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].idxmin()
+print("Best trade +"+str(round(dt.loc[dt['tradeIs'] == 'Good', 'resultat%'].max(),2)),"%, the ",dt['date'][idbest])
+print("Worst trade",round(dt.loc[dt['tradeIs'] == 'Bad', 'resultat%'].min(),2),"%, the ",dt['date'][idworst])
+print("Worst drawBack", str(100*round(dt['drawBack'].min(),2)),"%")
+print("Total fee : ",round(dt['frais'].sum(),2),"$")
+
+dt[['wallet','price']].plot(subplots=True, figsize=(12,10))
