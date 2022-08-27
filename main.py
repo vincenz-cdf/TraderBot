a5db30e9f7d572a350aed3f11070110a9fc09254
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-08-27 12:36:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import ftx
 import pandas as pd
-import ta
 import time
 import json
+import pickle
 from math import *
 
 accountName = ''
-pairSymbol = 'ETH/EUR'
-fiatSymbol = 'EUR'
+pairSymbol = 'ETH/USDT'
+fiatSymbol = 'USDT'
 cryptoSymbol = 'ETH'
 myTruncate = 3
+# open a pickle file
+filename = 'tradedata.pk'
 
 client = ftx.FtxClient(api_key='',
                    api_secret='', subaccount_name=accountName)
+
+#with open(filename, 'rb') as fi:
+#    purchasePrice_array = pickle.load(fi)
 
 data = client.get_historical_data(
     market_name=pairSymbol, 
@@ -23,11 +28,6 @@ data = client.get_historical_data(
     end_time=float(round(time.time())))
 df = pd.DataFrame(data)
 
-df['EMA28']=ta.trend.ema_indicator(df['close'], 28)
-df['EMA48']=ta.trend.ema_indicator(df['close'], 48)
-df['STOCH_RSI']=ta.momentum.stochrsi(df['close'])
-print(df)
-
 def getBalance(myclient, coin):
     jsonBalance = myclient.get_balances()
     if jsonBalance == []: 
@@ -37,20 +37,24 @@ def getBalance(myclient, coin):
         return 0
     else: 
         return float(pandaBalance.loc[pandaBalance['coin'] == coin]['total'])
-
+    
 def truncate(n, decimals=0):
     r = floor(float(n)*10**decimals)/10**decimals
     return str(r)
-    
+
+purchasePrice_array=[99999] #a retirer
+
 fiatAmount = getBalance(client, fiatSymbol)
 cryptoAmount = getBalance(client, cryptoSymbol)
 actualPrice = df['close'].iloc[-1]
 minToken = 5/actualPrice
 print(cryptoSymbol ,'price :', actualPrice, fiatSymbol ,'balance', fiatAmount , cryptoSymbol ,'balance :',cryptoAmount)
-
-if df['EMA28'].iloc[-2] > df['EMA48'].iloc[-2] and df['STOCH_RSI'].iloc[-2] < 0.8:
+    
+if actualPrice < purchasePrice_array[-1]-300.0 :
     if float(fiatAmount) > 5:
         quantityBuy = truncate(float(fiatAmount)/actualPrice, myTruncate)
+        purchasePrice = actualPrice
+        purchasePrice_array.append(purchasePrice)
         buyOrder = client.place_order(
             market=pairSymbol, 
             side="buy", 
@@ -60,9 +64,10 @@ if df['EMA28'].iloc[-2] > df['EMA48'].iloc[-2] and df['STOCH_RSI'].iloc[-2] < 0.
         print("BUY", buyOrder)
     else:
         print("If you  give me more USD I will buy more",cryptoSymbol)
-
-elif df['EMA28'].iloc[-2] < df['EMA48'].iloc[-2] and df['STOCH_RSI'].iloc[-2] > 0.2:
+elif actualPrice > purchasePrice_array[-1]+300.0:
     if float(cryptoAmount) > minToken:
+        purchasePrice = actualPrice
+        purchasePrice_array.append(purchasePrice)
         sellOrder = client.place_order(
             market=pairSymbol, 
             side="sell", 
@@ -73,4 +78,8 @@ elif df['EMA28'].iloc[-2] < df['EMA48'].iloc[-2] and df['STOCH_RSI'].iloc[-2] > 
     else:
         print("If you give me more",cryptoSymbol,"I will sell it")
 else :
-  print("No opportunity to take")
+      print("No opportunity to take")
+        
+with open(filename, 'wb') as fi:
+    # dump your data into the file
+    pickle.dump(purchasePrice_array, fi)
